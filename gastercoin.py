5bf9b9319f4fbf887dc6fddfb61164c50b982ca1
coizioc <36041795+coizioc@users.noreply.github.com>
2018-07-12 16:34:39 +0200

    Added blackjack and leaderboard commands.

--- gastercoin.py
+++ gastercoin.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 import random
 
 from subs.gastercoin import account as ac
-# from subs.gastercoin import blackjack as bj
+from subs.gastercoin import blackjack as bj
 from subs.gastercoin import deathmatch as dm
 from subs.gastercoin import quiz
 
@@ -20,6 +20,7 @@ with open(GASTERCOIN_CREATOR_ID_FILE, 'r') as f:
 PERMISSION_ERROR_STRING = f'Error: You do not have permission to use this command.'
 
 AUTHORIZED_CHANNELS = [340426498764832768, 408424622648721410, 340426332859269140, 464902493818585108]
+VIP_PEOPLE = [293219528450637824]
 
 
 class AmbiguousInputError(Exception):
@@ -47,6 +48,16 @@ def get_member_from_guild(guild_members, username):
             raise AmbiguousInputError([member.name for member in members])
 
 
+def parse_name(guild, username):
+    if '@' in username:
+        try:
+            return guild.get_member(int(username[3:-1]))
+        except:
+            raise NameError(username)
+    else:
+        return get_member_from_guild(guild.members, username)
+
+
 class Gastercoin():
     """Defines Gastercoin commands."""
 
@@ -64,6 +75,40 @@ class Gastercoin():
             await ctx.send(f"Added 1000 GasterCoins to {ctx.author.name}'s account!")
 
     @commands.command()
+    async def add(self, ctx, name, amount):
+        """Generates money out of thin air!"""
+        if ctx.author.id in VIP_PEOPLE:
+            try:
+                person_member = parse_name(ctx.guild, name)
+                amount = ac.parse_int(amount)
+                ac.update_account(person_member.id, amount)
+                amount_formatted = '{:,}'.format(amount)
+                await ctx.send(f"Added G${amount_formatted} to {person_member.name}'s account!")
+            except NameError:
+                await ctx.send(f'Error: {person} not found in server.')
+            except AmbiguousInputError as members:
+                await ctx.send(f'Error: input {person} can refer to multiple people ({members})')
+            except ValueError:
+                await ctx.send(f'Error: {amount} is invalid number.')
+
+    @commands.command()
+    async def remove(self, ctx, name, amount):
+        """Makes money disappear!"""
+        if ctx.author.id in VIP_PEOPLE:
+            try:
+                person_member = parse_name(ctx.guild, name)
+                amount = ac.parse_int(amount)
+                ac.update_account(person_member.id, -amount)
+                amount_formatted = '{:,}'.format(amount)
+                await ctx.send(f"Removed G${amount_formatted} from {person_member.name}'s account!")
+            except NameError:
+                await ctx.send(f'Error: {person} not found in server.')
+            except AmbiguousInputError as members:
+                await ctx.send(f'Error: input {person} can refer to multiple people ({members})')
+            except ValueError:
+                await ctx.send(f'Error: {amount} is invalid number.')
+
+    @commands.command()
     async def balance(self, ctx, name=None):
         """Checks the user's balance."""
         if name is None:
@@ -73,13 +118,53 @@ class Gastercoin():
             await ctx.send('As all things should be.')
         else:
             try:
-                person_member = get_member_from_guild(ctx.message.guild.members, name)
+                person_member = parse_name(ctx.message.guild, name)
                 amount = '{:,}'.format(ac.read_account(person_member.id))
                 await ctx.send(f'{person_member.name} has G${amount}')
             except NameError:
                 await ctx.send(f'Error: {name} not found in server.')
             except AmbiguousInputError as members:
                 await ctx.send(f'Error: input {name} can refer to multiple people ({members})')
+
+    @commands.command()
+    async def leaderboard(self, ctx, name=None):
+        """Allows users to easily compare each others' balances."""
+        leaderboard = ac.get_balances_by_amount()
+        out = '__**GasterCoin Leaderboard**__:\n'
+        if name is None:
+            for i in range(10):
+                user_id, amount = leaderboard[i]
+                amount_formatted = '{:,}'.format(amount)
+                out += f'**({1 + i}) {ctx.message.guild.get_member(user_id).name}**: G${amount_formatted}\n'
+            await ctx.send(out)
+        else:
+            if name == 'bottom':
+                for i in range(len(leaderboard) - 10, len(leaderboard)):
+                    user_id, amount = leaderboard[i]
+                    amount_formatted = '{:,}'.format(amount)
+                    out += f'**({1 + i}) {ctx.message.guild.get_member(user_id).name}**: G${amount_formatted}\n'
+                await ctx.send(out)
+            else:
+                try:
+                    name_list = [x[0] for x in leaderboard]
+                    name_member = parse_name(ctx.message.guild, name)
+                    name_index = name_list.index(name_member.id)
+                    if name_index < 5:
+                        lower = 0
+                        upper = 10
+                    else:
+                        lower = name_index - 5
+                        upper = name_index + 5
+                    if name_index + 5 > len(leaderboard):
+                        upper = len(leaderboard)
+                        lower = len(leaderboard) - 10
+                    for i in range(lower, upper):
+                        user_id, amount = leaderboard[i]
+                        amount_formatted = '{:,}'.format(amount)
+                        out += f'**({1 + i}) {ctx.message.guild.get_member(user_id).name}**: G${amount_formatted}\n'
+                    await ctx.send(out)
+                except ValueError:
+                    await ctx.send(f'Error: name {name} not found in leaderboard.')
 
     @commands.command()
     async def give(self, ctx, person, amount):
@@ -90,7 +175,7 @@ class Gastercoin():
             amount_formatted = '{:,}'.format(amount)
             person = person.lower()
             try:
-                person_member = get_member_from_guild(ctx.message.guild.members, person)
+                person_member = parse_name(ctx.message.guild, person)
                 ac.update_account(ctx.author.id, -amount)
                 ac.update_account(person_member.id, amount)
                 await ctx.send(f'G${amount_formatted} given to {person_member.name}!')
@@ -99,7 +184,7 @@ class Gastercoin():
             except AmbiguousInputError as members:
                 await ctx.send(f'Error: input {person} can refer to multiple people ({members})')
         else:
-            ctx.send(out)
+            await ctx.send(out)
 
     @commands.command(aliases=['flip'])
     async def flip_coin(self, ctx, bet, coin_side='h'):
@@ -111,14 +196,19 @@ class Gastercoin():
             if out == ac.SUCCESS_STRING:
                 if 'h' in coin_side or 't' in coin_side:
                     bet = ac.parse_int(bet)
-                    coin = random.randint(0, 1)
+                    cutoff = 50
+                    if 'h' in coin_side:
+                        cutoff -= 5
+                    else:
+                        cutoff += 5
+                    coin = random.randint(0, 100)
 
-                    if coin == 0:
+                    if coin <= cutoff:
                         picture = GASTERCOIN_HEAD_PICTURE
                     else:
                         picture = GASTERCOIN_TAILS_PICTURE
 
-                    if 'h' in coin_side and coin == 0 or 't' in coin_side and coin == 1:
+                    if 'h' in coin_side and coin <= cutoff or 't' in coin_side and coin > cutoff:
                         ac.update_account(ctx.author.id, bet)
                         out = "You won! "
                     else:
@@ -173,7 +263,7 @@ class Gastercoin():
         if out == ac.SUCCESS_STRING:
             bet = ac.parse_int(bet)
             try:
-                opponent_member = get_member_from_guild(ctx.message.guild.members, opponent)
+                opponent_member = parse_name(ctx.message.guild, opponent)
             except NameError:
                 await ctx.send(f'Error: {opponent} not found in server.')
                 return
@@ -216,7 +306,7 @@ class Gastercoin():
                     round_number = 1
 
                     while True:
-                        question_and_answer = quiz.get_question(tuple(('1')))
+                        question_and_answer = quiz.get_question(tuple('1'))
                         question = question_and_answer[0]
                         answer = question_and_answer[1]
                         header = f'__**Quiz Game Show Thing**__\n**' \
@@ -266,7 +356,7 @@ class Gastercoin():
             if out == ac.SUCCESS_STRING:
                 bet = ac.parse_int(bet)
                 try:
-                    opponent_member = get_member_from_guild(ctx.message.guild.members, opponent)
+                    opponent_member = parse_name(ctx.message.guild, opponent)
                 except NameError:
                     await ctx.send(f'Error: {opponent} not found in server.')
                     return
@@ -311,7 +401,7 @@ class Gastercoin():
                 await ctx.send(out)
         else:
             try:
-                opponent_member = get_member_from_guild(ctx.message.guild.members, opponent)
+                opponent_member = parse_name(ctx.message.guild, opponent)
             except NameError:
                 await ctx.send(f'Error: {opponent} not found in server.')
                 return
@@ -331,7 +421,8 @@ class Gastercoin():
             if args[0].lower() == 'edit':
                 attack = ' '.join(args[1:])
                 ac.update_account(ctx.author.id, attack, key=ac.SPECIAL_MOVE_KEY)
-                await ctx.send(f"{ctx.author.name}'s special move updated to: $P1 deals a heavy blow to $P2 by __**{attack}**__!!!")
+                await ctx.send(f"{ctx.author.name}'s special move updated to: $P1 deals a heavy blow to $P2 by __"
+                               f"**{attack}**__!!!")
             else:
                 await ctx.send(f"{ctx.author.name}'s current special move: $P1 deals a heavy blow to $P2 by "
                                f"__**{ac.read_account(ctx.author.id, key=ac.SPECIAL_MOVE_KEY)}**__!!!")
@@ -339,15 +430,59 @@ class Gastercoin():
             await ctx.send(f"{ctx.author.name}'s current special move: $P1 deals a heavy blow to $P2 by "
                            f"__**{ac.read_account(ctx.author.id, key=ac.SPECIAL_MOVE_KEY)}**__!!!")
 
-    @commands.command()
+    @commands.command(aliases=['bj'])
     async def blackjack(self, ctx, bet):
+        """Bets GasterCoins on a VERY SIMPLIFIED STUPIFIED VERSION of Blackjack."""
         out = ac.check_if_valid_transaction(ctx.author.id, bet)
         if out == ac.SUCCESS_STRING:
+            bet = ac.parse_int(bet)
+            bet_formatted = '{:,}'.format(bet)
             ac.update_account(ctx.author.id, -bet)
-            msg = await ctx.send('')
-            while True:
+            await ctx.send(f'*Game of Blackjack started by {ctx.author.name} with bet G${bet_formatted}*')
+            dealer_hand, player_hand = bj.init_hands()
+            if bj.get_value(player_hand) == 21:
+                ac.update_account(ctx.author.id, bet)
+                bet = round(bet * 1.5)
+                bet_formatted = '{:,}'.format(bet)
+                ac.update_account(ctx.author.id, bet)
+                out = bj.print_hands(dealer_hand, player_hand, ctx.author.name, len(dealer_hand))
+                out += f'**{ctx.author.name} has a blackjack and gets a payout of G${bet_formatted}!**'
+                await ctx.send(out)
+            else:
+                dealer_cards = 2
+                while True:
+                    out = bj.print_hands(dealer_hand, player_hand, ctx.author.name,
+                                         dealer_cards=dealer_cards, hide_dealer=True)
+                    out += 'Will you hit or stand?'
+                    await ctx.send(out)
 
-                msg.edit(content='')
+                    while True:
+                        message = await self.bot.wait_for('message')
+                        if message.author == ctx.author and \
+                                (bj.HIT in message.content.lower() or bj.STAND in message.content.lower()):
+                            break
+
+                    if bj.HIT in message.content.lower():
+                        player_hand = bj.hit(player_hand)
+                        if bj.get_value(player_hand) > 21:
+                            break
+                    else:
+                        break
+                    dealer_cards += 1
+
+                out = bj.print_hands(dealer_hand, player_hand, ctx.author.name, len(dealer_hand))
+                if bj.is_winner(player_hand, dealer_hand):
+                    ac.update_account(ctx.author.id, 2 * bet)
+                    out += f'**{ctx.author.name} has won G${bet_formatted}**!'
+                else:
+                    if bj.get_value(player_hand) > 21:
+                        out += f'**{ctx.author.name} has bust and lost G${bet_formatted}**'
+                    elif bj.get_value(player_hand) == bj.get_value(dealer_hand):
+                        ac.update_account(ctx.author.id, bet)
+                        out += f'**The hands are the same value and the bet of G${bet_formatted} has been pushed.**'
+                    else:
+                        out += f'**{ctx.author.name} has less than the dealer and lost G${bet_formatted}**'
+                await ctx.send(out)
         else:
             await ctx.send(out)
 
